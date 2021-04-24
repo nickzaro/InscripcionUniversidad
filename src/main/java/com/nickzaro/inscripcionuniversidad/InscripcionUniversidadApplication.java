094bc8a728d2a4ece16a341d9d87b8ebc7fea151
nickzaro <nickzaro@gmail.com>
2021-04-24 20:18:09 -0300

    Commit inicial

---
+++ src/main/java/com/nickzaro/inscripcionuniversidad/InscripcionUniversidadApplication.java
@@ -0,0 +1,13 @@
+package com.nickzaro.inscripcionuniversidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscripcionUniversidadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InscripcionUniversidadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nickzaro/inscripcionuniversidad/InscripcionUniversidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickzaro.inscripcionuniversidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscripcionUniversidadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
