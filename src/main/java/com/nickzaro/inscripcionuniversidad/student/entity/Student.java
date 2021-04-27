7783f32840d4399dfe0612de3b36ffb78c0c2cdb
nickzaro <nickzaro@gmail.com>
2021-04-27 18:59:21 -0300

    Definido el entity Student

--- src/main/java/com/nickzaro/inscripcionuniversidad/student/entity/Student.java
+++ src/main/java/com/nickzaro/inscripcionuniversidad/student/entity/Student.java
@@ -2,6 +2,22 @@ package com.nickzaro.inscripcionuniversidad.student.entity;
 
 import com.nickzaro.inscripcionuniversidad.share.entity.Person;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
 public class Student extends Person {
+
+    @Column(name = "student_id")
     private String studentID;
+
+
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
 }
