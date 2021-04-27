2aa1fcb9f6c93524e9475cd52789b4a2503c2967
nickzaro <nickzaro@gmail.com>
2021-04-27 13:11:48 -0300

    Agregado el horario a la vista de materias

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,10 +5,14 @@ INSERT INTO `courses`(id,name_of_course,total_quota,remaining_quota) values (2,'
 INSERT INTO `courses`(id,name_of_course,total_quota,remaining_quota) values (3,'Quimica I',30,20);
 
 /* Horarios*/
-INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (0,1,0,'08:00:00','10:00:00');
-INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (1,1,0,'10:00:00','12:00:00');
-INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (2,1,0,'12:00:00','14:00:00');
+INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (0,0,0,'08:00:00','10:00:00');
+INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (3,0,1,'08:00:00','10:00:00');
 
-INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (3,2,1,'08:00:00','10:00:00');
-INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (4,2,1,'10:00:00','12:00:00');
+INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (1,1,0,'10:00:00','12:00:00');
+INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (4,1,1,'10:00:00','12:00:00');
+
+INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (2,2,0,'12:00:00','14:00:00');
 INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (5,2,1,'12:00:00','14:00:00');
+
+INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (6,3,0,'12:00:00','14:00:00');
+INSERT INTO `schedules`(id,course_id,day_of_week,start_time,end_time) values (7,3,1,'08:00:00','10:00:00');
